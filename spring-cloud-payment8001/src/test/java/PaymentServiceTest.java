13e7240ac655258b274cd8bdd9d923b4f2ea7978
zhuqinghong <751277451@qq.com>
2020-08-10 21:53:34 +0800

    cloud2020 set up

---
+++ spring-cloud-payment8001/src/test/java/PaymentServiceTest.java
@@ -0,0 +1,11 @@
+import com.atguigu.springcloud.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PaymentServiceTest {
+    @Autowired
+    private PaymentService paymentService;
+
+
+}
---
+++ spring-cloud-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,41 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @PostMapping(value = {"/payment/create"})
+    public CommonResult create(@RequestBody Payment payment){
+        int result=paymentService.create(payment);
+        if(result>0){
+            return new CommonResult(200,"插入数据库成功"+serverPort,null);
+        }else{
+            return new CommonResult(444,"插入失败"+serverPort,null);
+        }
+
+    }
+
+    @GetMapping(value = {"/payment/get/{id}"})
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+
+        Payment payment = paymentService.getPaymentById(id);
+        if(payment!=null){
+            return new CommonResult(200,"查找数据库成功"+serverPort,payment);
+        }else{
+            return new CommonResult(444,"插入失败"+serverPort,null);
+        }
+    }
+}
